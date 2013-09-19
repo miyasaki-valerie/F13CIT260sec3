e77f2e5b952313911ceb97101139c28db4155bd7
Blankenship-Daniel <blankenshipdanielj@gmail.com>
2013-09-19 16:44:45 -0600

    GalToLitTable.java

---
+++ GalToLit/src/galtolit/GalToLit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galtolit;
+
+/**
+ *
+ * @author Daniel Blankenship
+ */
+public class GalToLit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double gallon;
+        double liters;
+        
+        gallon = 10;
+        liters = gallon * 3.7854;
+        System.out.println(gallon + " gallons is " + liters + " liters");
+    }
+}
