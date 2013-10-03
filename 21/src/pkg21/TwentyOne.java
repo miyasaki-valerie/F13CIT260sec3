6ad34fccd5ea18212158a93378f65a78f9d5e3fd
Blankenship-Daniel <blankenshipdanielj@gmail.com>
2013-10-03 14:33:55 -0600

    TwentyOne Paired Assignment1

--- 21/src/pkg21/TwentyOne.java
+++ 21/src/pkg21/TwentyOne.java
@@ -1,9 +1,29 @@
 package pkg21;
 
+import java.util.Scanner;
+
 public class TwentyOne {
         
+    String name;
+    String instructions = "\n\tWelcome to the game: **21**\n\n"
+            + "The instructions are as follows...\n";
+    
     public static void main(String[] args) {
-       Player newPlayer = new Player(); //create an instance of the class
-       newPlayer.displayStats();        //instance of the class & call the function
+      TwentyOne to = new TwentyOne();
+      to.getName();
+      to.displayHelp();
+      Player newPlayer = new Player(); //create an instance of the class
+      newPlayer.displayStats();        //instance of the class & call the function
     }
+    
+    public void getName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        this.name = input.next();
+    }
+    public void displayHelp() {
+        System.out.println("\nWelcome " + this.name + "\n");
+        System.out.println(this.instructions);
 }
+}
+
