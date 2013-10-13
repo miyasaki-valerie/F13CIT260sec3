bc23e79507dc1b91f30015fb20c722434fc08499
Valerie <Valerie@SakiTop.PK5001Z>
2013-10-12 22:53:14 -0700

    Hand Class changes

--- 21/src/pkg21/Hand.java
+++ 21/src/pkg21/Hand.java
@@ -9,5 +9,15 @@ import java.io.Serializable;
  * @author Daniel Blankenship
  */
 public class Hand implements Serializable{
-    
+    int card1;
+    int card2;
+    int card3;
+
+    public Hand(int card1, int card2, int card3) {
+        this.card1 = card1;
+        this.card2 = card2;
+        this.card3 = card3;
+    }
+
+   
 }
